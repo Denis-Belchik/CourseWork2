4dc7f2c06ef022a2b2ce9f995a390205d7cef7d2
Setlocal1989 <denis1989g@mail.ru>
2023-06-22 08:57:02 +0400

    init

---
+++ src/main/java/pro/sky/CourseWorkCore/CourseWorkCoreApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.CourseWorkCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWorkCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/CourseWorkCore/CourseWorkCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.CourseWorkCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
